175694be411c31e9bf117ba0c55ff09b80b48ad3
Narendra Sharma <nksharma514@gmail.com>
2025-04-14 15:59:29 +0530

    logo and color changed

--- src/app/pages/bangalore/bangalore.component.ts
+++ src/app/pages/bangalore/bangalore.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bangalore',
-  imports: [],
   templateUrl: './bangalore.component.html',
   styleUrl: './bangalore.component.css'
 })
