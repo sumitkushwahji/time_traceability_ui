0d55b47d1962a101335bb182bb7b698f6ee643da
Sumit Kushwah <sumitkushwahji@gmail.com>
2026-02-01 22:28:58 +0530

    Keycloak authentication functionality added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://rrsl.bharatsamay.org/time-traceability-service/api', // your prod API URL
+  keycloak: {
+    url: 'https://bharatsamay.org/auth',
+    realm: 'npl-realm',
+    clientId: 'frontend'
+  }
 };
